b8400b52b4e8f2980049f3edfcd49401ddf7c86f
TheMachin <thibaultmartinec@gmail.com>
2018-01-04 11:07:47 +0100

    Spring initializr

---
+++ src/main/java/barcodeproduct/service/BarCodeProduct/BarCodeProductApplication.java
@@ -0,0 +1,12 @@
+package barcodeproduct.service.BarCodeProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarCodeProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarCodeProductApplication.class, args);
+	}
+}
