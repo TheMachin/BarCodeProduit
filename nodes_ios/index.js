1e03b25436135bef5c192491a11ee97b3dd77d15
TheMachin <thibaultmartinec@gmail.com>
2018-01-12 22:32:03 +0100

    update nodes files

--- nodes_ios/index.js
+++ nodes_ios/index.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 8088;
+var serverPort = 8087;
 
 // swaggerRouter configuration
 var options = {
